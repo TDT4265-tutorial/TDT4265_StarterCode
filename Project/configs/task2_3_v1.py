2c0d5917da86e7ed2dc1e797b396864a687a9151
Eivind Dogger <eivind.dogger@gmail.com>
2022-04-25 20:33:13 +0200

    Further work on project

--- Project/configs/task2_3_v1.py
+++ Project/configs/task2_3_v1.py
@@ -1,5 +1,5 @@
 from math import gamma
-from ssd.modeling import backbones, SSDFocalLoss
+from ssd.modeling import backbones, SSDFocalLoss, AnchorBoxes
 from .tdt4265 import (
     train,
     optimizer,
@@ -10,11 +10,26 @@ from .tdt4265 import (
     val_cpu_transform,
     train_cpu_transform,
     gpu_transform,
-    label_map,
-    anchors
+    label_map
+    #anchors
 )
 from tops.config import LazyCall as L
 from ssd.modeling.backbones import FPN
+
+anchors = L(AnchorBoxes)(
+    feature_sizes=[[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
+    # Strides is the number of pixels (in image space) between each spatial position in the feature map
+    strides=[[4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]],
+    min_sizes=[[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
+    # aspect ratio is defined per feature map (first index is largest feature map (38x38))
+    # aspect ratio is used to define two boxes per element in the list.
+    # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
+    # Number of boxes per location is in total 2 + 2 per aspect ratio
+    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3]],
+    image_shape="${train.imshape}",
+    scale_center_variance=0.1,
+    scale_size_variance=0.2
+)
 
 backbone = L(FPN)(resnet_variant="resnet18",
                   pretrained=True,
